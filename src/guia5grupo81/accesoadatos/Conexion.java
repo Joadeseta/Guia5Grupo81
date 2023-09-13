d9549519cb39673b12b03af8a88b10cb0e60fc73
Joa Deseta <113145368+Joadeseta@users.noreply.github.com>
2023-09-13 11:04:26 -0300

    Agregando AlumnoData
    Agregamos alumnoData para enviar los datos de los alumnos a la base de datos con un método.

--- src/guia5grupo81/accesoadatos/Conexion.java
+++ src/guia5grupo81/accesoadatos/Conexion.java
@@ -9,6 +9,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 /**
  *
