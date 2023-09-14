cb26b14a9e53da0f2b6bcbd5712296b4b740e64a
Joa Deseta <113145368+Joadeseta@users.noreply.github.com>
2023-09-14 12:32:23 -0300

    Joa alumnoData
    Terminando alumnoData!

--- src/guia5grupo81/Guia5Grupo81.java
+++ src/guia5grupo81/Guia5Grupo81.java
@@ -17,16 +17,25 @@ import java.time.Month;
  */
 public class Guia5Grupo81 {
 
-   
     public static void main(String[] args) {
-        
-    Alumnos juan=new Alumnos(30303030,"Juan", "Perez", LocalDate.of(1980,5, 5),true );
-    
-    AlumnoData alu=new AlumnoData();
-    alu.guardarAlumno(juan);
-    
-    
-    
+
+        /*Alumnos juan = new Alumnos(1, 311111111, "Juan Pablo", "Perez", LocalDate.of(1980, 5, 5), true);*/
+        Alumnos Pedro = new Alumnos(7, 32222222, "Pedro Almo", "Perez", LocalDate.of(1980, 5, 5),true);
+        /*Alumnos Mari = new Alumnos(8, 33333333, "Mari", "Ladelbarrio", LocalDate.of(1980, 5, 5), true);
+        Alumnos Alicia = new Alumnos(9, 44444444, "Alicia", "Mara", LocalDate.of(1980, 5, 5), true);*/
+
+        AlumnoData alu = new AlumnoData();
+        /*
+        alu.guardarAlumno(juan);*/
+        /*alu.guardarAlumno(Pedro);*/
+        /*alu.guardarAlumno(Mari);
+        alu.guardarAlumno(Alicia);*/
+        /*alu.modificarAlumno(juan);*/
+        alu.modificarAlumno(Pedro);
+        /*alu.modificarAlumno(Mari);
+        alu.modificarAlumno(Alicia);*/
+        alu.eliminarAlumno(7);
+ 
     }
-    
+
 }
